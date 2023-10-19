c11b2b4246f4af8e17c86193965082cba4d088ac
Pablo784 <77360636+Pablo784@users.noreply.github.com>
2023-10-18 20:51:17 -0400

    This is the classwork we did on 10/18 and we are in the chapter of Functions

---
+++ Remain_Function.cpp
@@ -0,0 +1,16 @@
+/*
+I attempted today's classwork but the professor provided the solution which is the one below. Credit goes to the professor
+*/
+#include <iostream> 
+using namespace std;
+
+int remainder (int, int);
+
+int main() {
+
+cout << remainder(5,3) << endl;
+}
+int remainder (int a, int b)
+{
+  return a % b;
+}
