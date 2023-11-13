a501cf6d32be635f163da8fef46faf3e26a4c3a2
Pablo784 <77360636+Pablo784@users.noreply.github.com>
2023-11-13 15:46:48 -0500

    Classwork on vectors :unlock:
    Vectors are like arrays but more open

---
+++ Vector_v1_v2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+You attempted today's classwork (11/13) on vectors but the professor provided the solution in class and also explained the syntax. Credit goes to the profesor 
+*/
+
+/*
+int main ()  
+{
+
+  vector<int> v1(2);
+  vector <int> v2;
+
+v1[0] = 4;
+v1[1] = 5;
+
+  v2.push_back(4);
+  v2.push_back(4);
+  v2.push_back(3); 
+
+    cout << v1[0]<< "  " << v1[1]<< endl;
+    cout << v2[0]<< "  " << v2[1]<<"  " << v2[2] << endl;
+
+          if  ( v1 >= v2)
+      cout << "v1 is Greater than or Equal to v2\n\n ";
+    else
+      cout << "v1 is Not Greater than or Equal to v2\n\n " ;
+
+    cout << v2.size() << endl;
+
+          v2 = v1;
+
+      cout << v1[0]<< "  " << v1[1]<< endl;
+      cout << v2[0]<< "  " << v2[1]<<"  " <<  endl;
+
+          cout << v2.size() << endl;  // size decreases
+
+          if ( v1 == v2)
+      cout << "Equal\n\n";
+    else
+      cout << "Not Equal\n\n" ;
+
+  }
+*/
